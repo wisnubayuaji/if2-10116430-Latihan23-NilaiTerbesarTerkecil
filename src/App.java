6c3283421eb6212b665b3a698e244f24284e8219
wisnubayuaji <wisnubayu34@gmail.com>
2020-11-01 07:29:24 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author Nim : 10116430 
+ * Nama : Wisnu Bayu Aji 
+ * Kelas: IF-2
+ */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        double diameter = 0;
+        
+        do {
+            try {
+                
+                System.out.println("=====Perhitungan Lingkaran=====");
+                System.out.print("Masukan nilai diameter lingkaran : ");
+                diameter = sc.nextDouble();
+
+                System.out.println(diameter);
+            } catch (Exception e) {
+                //TODO: handle exception
+                
+            }
+        } while (diameter <= 0);
     }
 }
